4ae91f3e12730d1f6fe3b7d89c7fd456bbfde022
undefined2001 <asraful.islam.taj@gmail.com>
2025-02-09 14:01:43 +0600

    Adding FreeRTOS into it

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -21,10 +21,13 @@
 #include "uart.h"
 #include "model.hh"
 
+SemaphoreHandle_t xSamplingDone;
+SemaphoreHandle_t xProcessingDone;
+
 tflite::MicroErrorReporter micro_error_reporter;
 tflite::ErrorReporter *error_reporter = &micro_error_reporter;
 
-const tflite::Model *model = ::tflite::GetModel(temp_tflite);
+const tflite::Model *model = ::tflite::GetModel(temperature_quantized_tflite);
 
 using GestureOpResolver = tflite::MicroMutableOpResolver<2>;
 GestureOpResolver resolver;
@@ -51,6 +54,7 @@ void DebugCallback(const char *s)
 
 DHT dht{GPIOA, 1};
 float temp;
+
 int main(void)
 {
     SetSystemClock180MHz();
@@ -63,6 +67,9 @@ int main(void)
 
     TfLiteTensor *input;
     TfLiteTensor *output;
+
+    xSamplingDone = xSemaphoreCreateBinary();
+    xProcessingDone = xSemaphoreCreateBinary();
 
     // BuiltInBtnExtiInit();
 
@@ -92,7 +99,13 @@ int main(void)
 
     // vTaskStartScheduler();
     float scale = 0.00390625;
-    int zero_point = 128;
+    int zero_point = -128;
+    float predictions[3];
+
+    int class_index = 0;
+    float max_prob = predictions[0];
+
+    float xscaled;
 
     while (1)
     {
@@ -104,7 +117,9 @@ int main(void)
             printf("[ERROR] Input tensor is NULL!\n");
         }
 
-        input->data.int8[0] = static_cast<int8_t>(temp);
+        xscaled = (static_cast<int>(temp) - 21.7625) / 6.1161;
+
+        input->data.int8[0] = static_cast<int8_t>((xscaled / 0.01795339770615101) - 12);
 
         printf("Input tensor dims: %d\n\r", input->dims->size);
         printf("Input tensor type: %d\n\r", input->type);
@@ -122,12 +137,34 @@ int main(void)
             printf("[ERROR] Output tensor is NULL!\n");
         }
 
-        // Dequantize and print results
+        // Print raw output probabilities
+        printf("Raw Model Output:\n\r");
+        printf("Cold  [0]: %d\n\r", output->data.int8[0]);
+        printf("Warm  [1]: %d\n\r", output->data.int8[1]);
+        printf("Hot   [2]: %d\n\r", output->data.int8[2]);
+
         for (int i = 0; i < 3; i++)
         {
-            float real_value = scale * (output->data.int8[i] + zero_point);
-            printf("Output[%d]: %.6f\n\r", i, real_value);
+            predictions[i] = scale * (output->data.int8[i] - zero_point);
         }
+        printf("Dequantized Model Output:\n\r");
+        printf("Cold  [0]: %.6f\n\r", predictions[0]);
+        printf("Warm  [1]: %.6f\n\r", predictions[1]);
+        printf("Hot   [2]: %.6f\n\r", predictions[2]);
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (predictions[i] > max_prob)
+            {
+                max_prob = predictions[i];
+                class_index = i;
+            }
+        }
+
+        const char *labels[] = {"Cold", "Warm", "Hot"};
+        printf("Predicted: %s (Confidence: %.2f)\n\r", labels[class_index], max_prob);
+        printf("--------------------------------------------------\n\r");
+
         DelayMS(2000);
     }
 
@@ -156,3 +193,23 @@ extern "C" int __io_putchar(int ch)
     UART_SendChar(USART2, ch);
     return ch;
 }
+
+void pvDataReadingTask(void *params)
+{
+    //Task's Program goes here
+    xSemaphoreGive(xSamplingDone);
+};
+
+void pvDataProcessingTask(void *params)
+{
+    xSemaphoreTake(xSamplingDone, portMAX_DELAY);
+
+    // Tasks program goes here
+    xSemaphoreGive(xProcessingDone);
+};
+
+void pvDataLoggingTask(void *params)
+{
+    xSemaphoreTake(xProcessingDone, portMAX_DELAY);
+    // Tasks program goes here
+};
